f086f891af65efc89e1bd4c0eebf64d1ecb3df19
Sharif Salah <sharif.salah@gmail.com>
2015-10-16 22:48:38 +0100

    Initial commit

---
+++ settings.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""settings.py
+
+server-side Python App Engine app user settings
+
+$Id$
+
+"""
+
+# Replace the following lines with client IDs obtained from the APIs
+# Console or Cloud Console.
+WEB_CLIENT_ID = 'replace with Web client ID'
+
