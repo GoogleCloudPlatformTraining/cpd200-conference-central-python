ae093f814842591324b5f4a97ab28255672386cd
Sharif Salah <sharif.salah@gmail.com>
2015-11-19 06:18:10 -0800

    additions and solutions
    Some reorganisation to match course structure.
    Added a reference solution, REF_9_conference.py

--- additions/TODO_9_conference.py
+++
@@ -1,8 +0,0 @@
-        # Look for TODO 2
-        # create Conference, send email to organizer confirming
-        # creation of Conference & return (modified) ConferenceForm
-        Conference(**data).put()
-        taskqueue.add(params={'email': user.email(),
-            'conferenceInfo': repr(request)},
-            url='/tasks/send_confirmation_email'
-        )
